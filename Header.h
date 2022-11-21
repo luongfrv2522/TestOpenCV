188990c644e2ddcbd8b835b0d0073fa1a994c560
luonghv <luongfrv2522@gmail.com>
2022-11-21 18:15:20 +0700

    d

--- Header.h
+++ Header.h
@@ -8,8 +8,10 @@
 using namespace cv;
 using namespace std;
 
-Scalar green = Scalar(0, 255, 0, 255);
-Scalar blue = Scalar(255, 0, 0, 255);
+Scalar BEP_SCALAR_GREEN = Scalar(0, 255, 0, 255);
+Scalar BEP_SCALAR_BLUE = Scalar(255, 0, 0, 255);
+Scalar BEP_SCALAR_RED = Scalar(0, 0, 255, 255);
+Scalar BEP_SCALAR_YELLOW = Scalar(0, 255, 255, 255);
 
 struct Left_Right_contour_sorter // 'less' for contours
 {
@@ -55,7 +57,7 @@ void persTrans(vector<vector<Point>> &inputMat, vector<vector<Point>> &dst, Mat&
 	}
 }
 
-void myDrawContours(Mat& src, vector<vector<Point>> contours, bool hasLabel = false) {
+void myDrawContours(Mat& src, vector<vector<Point>> contours, Scalar color, bool hasLabel) {
 	int count = 0;
 	for (size_t i = 0; i < contours.size(); i++) {
 		count++;
@@ -63,11 +65,17 @@ void myDrawContours(Mat& src, vector<vector<Point>> contours, bool hasLabel = fa
 		//
 		Point point4 = Point((r.br() + r.tl()) * .5);
 		circle(src, point4, 1, 255, 3);
-		rectangle(src, r, green, 2, 8, 0);
-		String text = to_string(count) + "-" + to_string(r.area());
+		rectangle(src, r, color, 2, 8, 0);
+		String text = to_string(count);
 		cout << text << endl;
 		if (hasLabel) {
-			putText(src, text, point4, FONT_HERSHEY_SIMPLEX, 0.5, blue, 2);
+			putText(src, text, point4, FONT_HERSHEY_SIMPLEX, 0.5, BEP_SCALAR_BLUE, 2);
 		}
 	}
+}
+void myDrawContours(Mat& src, vector<vector<Point>> contours, bool hasLabel) {
+	myDrawContours(src, contours, BEP_SCALAR_GREEN, hasLabel);
+}
+void myDrawContours(Mat& src, vector<vector<Point>> contours) {
+	myDrawContours(src, contours, BEP_SCALAR_GREEN, false);
 }
--- TestOpenCV.cpp
+++ TestOpenCV.cpp
@@ -11,26 +11,43 @@ int main()
     //TestFindCircle::Run();
 
     //TestFindCircle2::Run();
-
-    float w = 768;
-    float h = 1024;
+    
+    //
+    float w = 768, h = 1024;
+    float wShow = 576, hShow = 768;
     Mat img = imread("Resources/50_cau.jpg");
+    //
+    float hCodeVsAnswer = h/3;
     Mat imgReSize, imgGray, imgThreshHold, imgDilate, outMat;
     //
     resize(img, img, Size(w, h));
 
+    #pragma region show Image2.1
+    /*resize(img, imgReSize, Size(wShow, hShow));
+    imshow("Image1.1", imgReSize);*/
+    #pragma endregion
+
     cvtColor(img, imgGray, COLOR_BGR2GRAY);
     adaptiveThreshold(imgGray, imgThreshHold, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY, 45, 5);
-    //imshow("Image2.1", imgThreshHold);
+    #pragma region show Image2.1
+    /*resize(imgThreshHold, imgReSize, Size(wShow, hShow));
+    imshow("Image2.1", imgReSize);*/
+    #pragma endregion
     //Giãn nền
     Mat elementDilate = getStructuringElement(MORPH_CROSS, Size(3, 3), Point(1, 1));
     dilate(imgThreshHold, imgDilate, elementDilate, Point(-1, -1), 3);
-    //imshow("Image3.1", imgDilate);
+    #pragma region show Image2.1
+    /*resize(imgDilate, imgReSize, Size(wShow, hShow));
+    imshow("Image3.1", imgReSize);*/
+    #pragma endregion
     Mat elementErode = getStructuringElement(MORPH_RECT, Size(3, 3), Point(1, 1));
     erode(imgDilate, imgDilate, elementErode, Point(-1, -1), 3);
-    //imshow("Image3.2", imgDilate);
+    #pragma region show Image2.1
+    /*resize(imgDilate, imgReSize, Size(wShow, hShow));
+    imshow("Image3.2", imgReSize);*/
+    #pragma endregion
 
-    //Tìm viền các hình chữ nhật neo góc
+    //Tìm viền các hình tô đậm
     vector<vector<Point>> contours;
     findContours(imgDilate, contours, RETR_TREE, CHAIN_APPROX_NONE);
 
@@ -44,12 +61,10 @@ int main()
             contoursToSort.push_back(contours[i]);
         }
     }
-    //drawToContour(img, contoursToSort);
-    //imshow("Image3.3", img);
-
+    #pragma region Find contours
+    vector<vector<Point>> contours4BasePoint;
+    vector<vector<Point>> contoursSubBasePoint;
     if (contoursToSort.size() >= 14) {
-        vector<vector<Point>> contours4BasePoint;
-        vector<vector<Point>> contoursSubBasePoint;
         sort(contoursToSort.begin(), contoursToSort.end(), area_desc_contour_sorter());
         for (size_t i = 0; i < contoursToSort.size(); i++) {
             if (i < 4) {
@@ -93,13 +108,106 @@ int main()
         //drawToContour(outMat, contours4BasePoint);
 
         sort(contoursSubBasePoint.begin(), contoursSubBasePoint.end(), Top_Bottom_contour_sorter());
-        myDrawContours(outMat, contoursSubBasePoint, true);
+        #pragma region Chia phần câu hỏi và mã đề
+        vector<vector<Point>> contoursSubCodeBasePoint;
+        vector<vector<Point>> contoursSubAnswerBasePoint;
+        for (size_t i = 0; i < contoursSubBasePoint.size(); i++) {
+			if (contoursSubBasePoint[i][0].y < hCodeVsAnswer) {
+                contoursSubCodeBasePoint.push_back(contoursSubBasePoint[i]);
+            }
+            else {
+                contoursSubAnswerBasePoint.push_back(contoursSubBasePoint[i]);
+            }
+        }
+        myDrawContours(outMat, contoursSubCodeBasePoint, BEP_SCALAR_YELLOW, false);
+        myDrawContours(outMat, contoursSubAnswerBasePoint, BEP_SCALAR_GREEN, false);
+        #pragma endregion
+
+        #pragma region Tìm hình chữ nhật cơ sở của số báo danh và mã đề
+        vector<Rect> subCodeBase4Point;
+        subCodeBase4Point.push_back(boundingRect(contoursSubCodeBasePoint[0]));
+        subCodeBase4Point.push_back(boundingRect(contoursSubCodeBasePoint[1]));
+        subCodeBase4Point.push_back(boundingRect(contoursSubCodeBasePoint[contoursSubCodeBasePoint.size()-2]));
+        subCodeBase4Point.push_back(boundingRect(contoursSubCodeBasePoint[contoursSubCodeBasePoint.size()-1]));
+        //
+        Mat identityMat = outMat(Rect(
+            subCodeBase4Point[0].br().x,
+            subCodeBase4Point[0].br().y,
+            subCodeBase4Point[0].width * 9,
+            subCodeBase4Point[2].tl().y - subCodeBase4Point[0].br().y
+        ));
+        /*Mat codeMat = outMat(Rect(
+            subCodeBase4Point[1].br().x,
+            subCodeBase4Point[1].br().y,
+            subCodeBase4Point[1].width * 5,
+            subCodeBase4Point[3].br().y - subCodeBase4Point[1].br().y
+        ));*/
+
+        #pragma region show Image3.5
+        /*for (int i = 0; i < 10; i++) {
+            line(identityMat,
+                Point(0, identityMat.size().height / 10 * (i+1)),
+                Point(identityMat.size().height, identityMat.size().height / 10 * (i + 1)),
+                BEP_SCALAR_BLUE,
+                1,
+                LINE_8
+            );
+        }*/
+        //Tìm viền các hình tô đậm
+        Mat identityMatTempThesh;
+        cvtColor(identityMat, identityMatTempThesh, COLOR_BGR2GRAY);
+        threshold(identityMatTempThesh, identityMatTempThesh, 0, 255,THRESH_BINARY_INV | THRESH_OTSU);
+        //
+        vector<vector<Point>> codeContours;
+        vector<vector<Point>> codeContoursToSort;
+        findContours(identityMatTempThesh, codeContours, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
+        imshow("Image3.5", identityMatTempThesh);
+        for (size_t i = 0; i < codeContours.size(); i++) {
+            Rect r = boundingRect(codeContours[i]);
+            bool flagRect = r.width / (double)r.height < 1.5 && r.width / (double)r.height > 0.75 && r.area() < 1000 && r.area() > 50;
+            if (flagRect) {
+                codeContoursToSort.push_back(codeContours[i]);
+            }
+        }
+        sort(codeContoursToSort.begin(), codeContoursToSort.end(), Left_Right_contour_sorter());
+        int roundLine = 10;
+        String identityNum;
+        for (size_t i = 0; i < codeContoursToSort.size(); i = i + roundLine) {
+            int tempVal = -1;
+            int indexIdentityNum = 0;
+            sort(codeContoursToSort.begin() + i, codeContoursToSort.begin() + i + roundLine, Top_Bottom_contour_sorter());
+            for (size_t j = i; j < i + roundLine; j++) {
+                Rect boundCodeContours = boundingRect(codeContoursToSort[j]);
+                Mat mask = Mat(boundCodeContours.height, boundCodeContours.width, uchar(0));
+                fillPoly(mask, codeContoursToSort, Scalar(255), 8, 0, -boundCodeContours.tl());
+                Mat cropped = identityMatTempThesh(boundCodeContours) & mask;
+                int countNonZe = countNonZero(cropped);
+                if (countNonZe > (boundCodeContours.width * boundCodeContours.height) / 2) {
+                    tempVal = indexIdentityNum;
+                }
+                indexIdentityNum++;
+            }
+            identityNum = identityNum +"|" + to_string(tempVal);
+        }
+
+        //myDrawContours(identityMat, codeContoursToSort);
+
+        putText(outMat, identityNum, subCodeBase4Point[0].br(), FONT_HERSHEY_SIMPLEX, 0.5, BEP_SCALAR_BLUE, 2);
+        imshow("Image3.6", identityMat);
+        //imshow("Image3.6", codeMat);
+        #pragma endregion show Image3.5
+        #pragma endregion
     }
     else if (contoursToSort.size() < 4) {
         cout << "---------------------------------" << endl;
         cout << "WARNING NO FOUND 4Point " << contours.size() << endl;
     }
+    #pragma endregion
 
-    imshow("Image4", outMat);
+    #pragma region show Image2.1
+    line(outMat, Point(0, hCodeVsAnswer), Point(w, hCodeVsAnswer), BEP_SCALAR_BLUE, 5, LINE_8);
+    resize(outMat, imgReSize, Size(wShow, hShow));
+    imshow("Image4", imgReSize);
+    #pragma endregion
     waitKey(0);
 }
